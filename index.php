fbaedec4fcf986df6fb24828bbd29e954987554d
waew-waw <jutarat.sop@ku.th>
2023-01-09 12:21:32 +0700

    Add britday

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <form action="show.php" method="post">
         what is your name:<input type="text" name="name"><br>
         what is your age:<input type="text" name="age"><br>
+        What is you birtday<input type="text" name="britday"><br>
         <input type="submit" value="Submit">
 
     </form>
