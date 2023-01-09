e5aff1c82bb1c0a31ddb49ab067f780966f07388
waew-waw <jutarat.sop@ku.th>
2023-01-09 12:19:18 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <form action="show.php" method="post">
+        what is your name:<input type="text" name="name"><br>
+        what is your age:<input type="text" name="age"><br>
+        <input type="submit" value="Submit">
+
+    </form>
+</body>
+</html>
