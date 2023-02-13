ed3964ae524178a0b8974137ca1d502b85893b26
waew-waw <jutarat.sop@ku.th>
2023-02-13 10:01:05 +0700

    01

--- list_people.php
+++ list_people.php
@@ -7,7 +7,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    if($row["sex"]=="m"){
+    if($row["sex"]=="M"){
         if($row["age"]>=15){
             echo "นาย".$row["fname"]." ".$row["lname"]." <a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
         }
@@ -16,7 +16,7 @@ if ($result->num_rows > 0) {
         }
     }else{
         if($row["age"]>=15){
-            if($row["marry_status"]=="m"){
+            if($row["marry_status"]=="M"){
                 echo "นาง".$row["fname"]." ".$row["lname"]." <a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
             }else{
                 echo "น.ส.".$row["fname"]." ".$row["lname"]." <a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
