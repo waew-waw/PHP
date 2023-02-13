9c85560948227d6d4f4c4d888cae07d6332e1a18
waew-waw <jutarat.sop@ku.th>
2023-02-13 11:07:43 +0700

    00

--- list_people.php
+++ list_people.php
@@ -1,7 +1,23 @@
 <?php
 require("connect_db.php");
 
-$sql = "SELECT id, fname, lname, age, sex, marry_status FROM survey";
+if(isset($_GET["keyword"])){
+    $keyword=$_GET["keyword"];
+}else{
+    $keyword="";
+}
+
+if($keyword!=""){
+    $sql = "SELECT id, fname, lname, age, sex, marry_status FROM survey WHERE fname LIKE '%$keyword%' OR lname LIKE '%$keyword%'";
+}else{
+    $sql = "SELECT id, fname, lname, age, sex, marry_status FROM survey";
+}
+?>
+<form action="list_people.php" method="get">
+    Search:<input type="text" name="keyword" value="<?php print($keyword);?>">
+    <input type="submit" value="Search">
+</form><br>
+<?php
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -9,27 +25,29 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
     if($row["sex"]=="M"){
         if($row["age"]>=15){
-            echo "นาย".$row["fname"]." ".$row["lname"]." <a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
+            echo "นาย".$row["fname"]." ".$row["lname"];
         }
         else{
-            echo "ด.ช.".$row["fname"]." ".$row["lname"]." <a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
+            echo "ด.ช.".$row["fname"]." ".$row["lname"];
         }
     }else{
         if($row["age"]>=15){
             if($row["marry_status"]=="M"){
-                echo "นาง".$row["fname"]." ".$row["lname"]." <a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
+                echo "นาง".$row["fname"]." ".$row["lname"];
             }else{
-                echo "น.ส.".$row["fname"]." ".$row["lname"]." <a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
+                echo "น.ส.".$row["fname"]." ".$row["lname"];
             }
-           
+            
         }
         else{
-            echo "ด.ญ.".$row["fname"]." ".$row["lname"]." <a href='edit_people.php?id=".$row["id"]."'>Edit</a><br>";
+            echo "ด.ญ.".$row["fname"]." ".$row["lname"];
         }
     }
+    echo " <a href='edit_people.php?id=".$row["id"]."'>Edit</a> <a href='delete_people.php?id=".$row["id"]."' onclick=\"return confirm('Are you sure to delete ".$row["fname"]."')\".>Delete</a><br>";
   }
 } else {
   echo "0 results";
 }
 $conn->close();
 ?>
+<a href="add_people.php">Add People</a>
---
+++ save_add_people.php
@@ -0,0 +1,15 @@
+<?php
+require("connect_db.php");
+
+$fname=$_POST["fname"];
+$lname=$_POST["lname"];
+$age=$_POST["age"];
+$sex=$_POST["sex"];
+$marry_status=$_POST["marry_status"];
+
+//echo "fname:$fname lname:$lname age:$age sex:$sex marry_status:$marry_status";
+$sql="INSERT INTO survey(fname,lname,age,sex,marry_status)";
+$sql.=" VALUES('$fname','$lname','$age','$sex','$marry_status')";
+$conn->query($sql);
+header( "location: list_people.php" );
+?>
